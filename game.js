b78d8340a69620f8820f743cf1e871b7efd5bdf2
Brian Nippert <briannippert@gmail.com>
2018-09-24 17:11:39 -0400

    Added mouse event handeling that returns the grid of the mouse

--- game.js
+++ game.js
@@ -4,40 +4,83 @@ canvas.height = 800;
 var ctx = canvas.getContext('2d');
 var chips = [];
 
-class chip{
-    constructor(row,col,color)
-    {
-        this.row = row;
-        this.col = col;
-        this.radius = 30;
-        this.color = color;
-          this.y = (this.col * 90) + 85;
-          this.x = (this.row * 90) + 85; 
-        this.draw();
-        chips.push(this);
+class pos {
+  constructor(row, col) {
+    this.row = row;
+    this.col = col;
+  }
+  getRow() {
+    return this.row
+  }
+  getCol() {
+    return this.col
+  }
+}
+
+class chip {
+  constructor(row, col, color) {
+    this.row = row;
+    this.col = col;
+    this.radius = 30;
+    this.color = color;
+    this.y = (this.col * 90) + 85;
+    this.x = (this.row * 90) + 85;
+    this.draw();
+    chips.push(this);
+  }
+  flip() {
+    if (this.color == "red") {
+      this.color = "white"
+    } else {
+      this.color = "red"
     }
-    flip()
-    {
-      if(this.color == "red")
-      {
-        this.color ="white"
-      }
-      else
-      {
-        this.color ="red"
-      }
-     this.draw();
-    }
-    draw()
-    {
+    this.draw();
+  }
+  draw() {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     ctx.closePath();
     ctx.fillStyle = this.color;
-    ctx.fill();  
-    }
+    ctx.fill();
+  }
 }
 
+function placeChip(col, row, color) {
+
+}
+
+function highlight(row, col) {
+
+}
+
+
+function handleMouseMove(e) {
+  var mouseX = e.clientX;
+  var mouseY = e.clientY;
+  getGridNumber(mouseX, mouseY);
+
+}
+
+function getGridNumber(mouseX, mouseY) {
+  var col = Math.floor((mouseX + 40) / 90)
+  var row = Math.floor((mouseY + 40) / 90);
+  col--;
+  row--;
+  if (col > 7) {
+    col = 7;
+  }
+  if (row > 7) {
+    col = 7;
+  }
+  if (col < 0) {
+    col = 0;
+  }
+  if (row < 0) {
+    col = 0;
+  }
+  console.log("[" + col + "," + row + "]");
+  var position = new pos(col, row);
+}
 
 function clear() {
   ctx.fillStyle = 'rgba(0, 0, 0, 1)';
@@ -63,14 +106,17 @@ function remove(array, element) {
   }
 }
 
-function drawGrid(){
+function drawGrid() {
   const p = 40;
 
-  const bw = canvas.width - (p * 2) ;
-  const bh = canvas.height - (p * 2) ;
+  const bw = canvas.width - (p * 2);
+  const bh = canvas.height - (p * 2);
 
-  console.log({bw,bh})
-  for (let x = 0; x <= bw; x += 90){
+  console.log({
+    bw,
+    bh
+  })
+  for (let x = 0; x <= bw; x += 90) {
     ctx.moveTo(0.5 + x + p, p);
     ctx.lineTo(0.5 + x + p, bh + p);
   }
@@ -85,15 +131,15 @@ function drawGrid(){
 }
 clear();
 drawGrid();
-new chip(0,2,"red")
-new chip(0,0,"red")
-new chip(2,0,"red")
-new chip(2,2,"red")
-new chip(0,2,"red")
-new chip(3,1,"red")
-new chip(2,0,"red")
-new chip(7,4,"red")
-new chip(3,2,"red")
-new chip(4,0,"red")
-new chip(2,6,"red")
-new chip(7,7,"red")
+new chip(0, 2, "red")
+new chip(0, 0, "red")
+new chip(2, 0, "red")
+new chip(2, 2, "red")
+new chip(0, 2, "red")
+new chip(3, 1, "red")
+new chip(2, 0, "red")
+new chip(7, 4, "red")
+new chip(3, 2, "red")
+new chip(4, 0, "red")
+new chip(2, 6, "red")
+new chip(7, 7, "red")
